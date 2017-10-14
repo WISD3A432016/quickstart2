3e7dc14b6be1a85e92195d93e92d7629e359a757
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-14 08:48:24 +0800

    @include('common.errors')

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ABC
- * Date: 2017/10/14
- * Time: 上午 08:40
- */
+// resources/views/common/errors.blade.php
+
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
