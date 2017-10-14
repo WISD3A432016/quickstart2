6e3e458331870c3658cca1b9da5d0618c266122d
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-14 09:12:54 +0800

    Connect Task model and TaskPolicy -> app/Providers/AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,12 +8,12 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
+     * 應用程式的原則對應。
      *
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        Task::class => TaskPolicy::class,
     ];
 
     /**
