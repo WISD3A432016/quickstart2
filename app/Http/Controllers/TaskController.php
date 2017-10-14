677ea16587649773359e57e3cdb79491d1826e05
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-14 09:03:47 +0800

    inject Repository

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,34 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
     /**
-     * 建立一個新的控制器實例。
+     * 任務資源庫的實例。
      *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+
+    /**
+     * 建立新的控制器實例。
+     *
+     * @param  TaskRepository  $tasks
      * @return void
      */
-    public function __construct()
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+
+        $this->tasks = $tasks;
     }
 
     /**
-     * 顯示使用者所有任務的清單。
+     * 取得給定使用者的所有任務。
      *
      * @param  Request  $request
      * @return Response
      */
     public function index(Request $request)
     {
-        $tasks = Task::where('user_id', $request->user()->id)->get();
-
         return view('tasks.index', [
-            'tasks' => $tasks,
+            'tasks' => $this->tasks->forUser($request->user()),
         ]);
     }
 
@@ -52,4 +63,6 @@ class TaskController extends Controller
 
         return redirect('/tasks');
     }
+
+
 }
