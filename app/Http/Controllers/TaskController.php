99158abf8210c6be4f2f71ae31bea8a7655bb72b
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-10-14 08:50:40 +0800

    TaskController@store -> create new tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -42,5 +42,10 @@ class TaskController extends Controller
         ]);
 
         // Create The Task...
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
     }
 }
